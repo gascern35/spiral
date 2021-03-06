44160742cae6fbbb1f2d043674fc2c5406706834
Gaspar Cernacek <gasparcernacek@gmail.com>
2021-03-06 12:38:49 -0500

    added third vertex

--- sketch.js
+++ sketch.js
@@ -1,52 +1,48 @@
 let bigAngle;
+let midAngle;
 let smallAngle;
+
 let bigR;
+let midR;
 let smallR;
+
 let bigSlider;
+let midSlider;
 let smallSlider;
-let bigCheck;
-let smallCheck;
 
 function setup() {
   colorMode(HSB);
-  createCanvas(600, 600);
+  createCanvas(800, 600);
   bigSlider = createSlider(-90, 90, 2);
+  midSlider = createSlider(-90, 90, 2);
   smallSlider = createSlider(-90, 90, 2);
-  //bigCheck = createCheckbox('REVERSE', false);
-  //smallCheck = createCheckbox('reverse', false);
-  smallR = 125;
   bigR = 250;
   bigAngle = PI / 2;
+  smallR = bigR / 2;
   smallAngle = PI / 2;
+  midR = smallR * 1.5;
+  midAngle = PI;
 }
 
 function draw() {
   translate(width / 2, height / 2);
   background(45, 80, 90, 0.08);
-  strokeWeight(2);
-  noFill();
+  strokeWeight(4);
   stroke(70, 90, 90, 0.06);
-  noStroke();
-  ellipse(0, 0, bigR * 2);
-  let bigx = bigR * cos(bigAngle);
-  let bigy = bigR * sin(bigAngle);
-  ellipse(0, 0, smallR * 2);
   let smallx = smallR * cos(smallAngle);
   let smally = smallR * sin(smallAngle);
+  let midx = midR * cos(midAngle);
+  let midy = midR * sin(midAngle);
+  let bigx = bigR * cos(bigAngle);
+  let bigy = bigR * sin(bigAngle);
   stroke(200, 100, 90);
   beginShape(LINES);
-  vertex(bigx, bigy);
   vertex(smallx, smally);
+  vertex(midx, midy);
+  vertex(midx, midy);
+  vertex(bigx, bigy);
   endShape();
-  //if (bigCheck.checked()) {
-  //bigAngle -= 0.01 * bigSlider.value();
-  //} else {
-  //}
-
-  //if (smallCheck.checked()) {
-  //smallAngle -= 0.01 * smallSlider.value();
-  //} else {
-  //}
-  bigAngle += 0.01 * bigSlider.value();
   smallAngle += 0.01 * smallSlider.value();
+  midAngle += 0.01 * midSlider.value();
+  bigAngle += 0.01 * bigSlider.value();
 }
