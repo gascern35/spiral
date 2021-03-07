6572da2f922517629f84fe82f3ee6daf728d4cd8
Gaspar Cernacek <gasparcernacek@gmail.com>
2021-03-06 19:38:24 -0500

    added the ability to change the background color

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+let val = [10, 100, 60, 0.08];
+let colorB;
 let bigAngle;
 let midAngle;
 let smallAngle;
@@ -13,6 +15,8 @@ let smallSlider;
 function setup() {
   colorMode(HSB);
   createCanvas(800, 600);
+  background(45, 80, 90, 0.08);
+  colorB = createButton('COLOR');
   bigSlider = createSlider(-90, 90, 2);
   midSlider = createSlider(-90, 90, 2);
   smallSlider = createSlider(-90, 90, 2);
@@ -22,20 +26,23 @@ function setup() {
   smallAngle = PI / 2;
   midR = smallR * 1.5;
   midAngle = PI;
+  colorB.mousePressed(changeBG);
 }
 
+function changeBG() {
+val = [random(0, 360), random(90,100), 60, 0.08];
+}
 function draw() {
+  background(val[0], val[1], val[2], val[3]);
   translate(width / 2, height / 2);
-  background(45, 80, 90, 0.08);
   strokeWeight(4);
-  stroke(70, 90, 90, 0.06);
+  stroke(40, 90, 90);
   let smallx = smallR * cos(smallAngle);
   let smally = smallR * sin(smallAngle);
   let midx = midR * cos(midAngle);
   let midy = midR * sin(midAngle);
   let bigx = bigR * cos(bigAngle);
   let bigy = bigR * sin(bigAngle);
-  stroke(200, 100, 90);
   beginShape(LINES);
   vertex(smallx, smally);
   vertex(midx, midy);
