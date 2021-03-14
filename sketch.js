d58f1e156d7cee0c047c347f82d2ce8ef77ef799
Gaspar Cernacek <gasparcernacek@gmail.com>
2021-03-13 22:49:39 -0500

    added curves

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-let rand;
-let val = [10, 100, 60, 0.09];
+let backgroundVal = [10, 100, 60, 0.09];
+let lineVal = [60, 100, 100]
 let colorB;
 
 let bigAngle;
@@ -17,7 +17,7 @@ let smallSlider;
 function setup() {
   colorMode(HSB);
   createCanvas(800, 600);
-  background(45, 80, 90, 0.09);
+  curveTightness(-5);
   colorB = createButton('COLOR');
   bigSlider = createSlider(-20, 20, 0);
   midSlider = createSlider(-20, 20, 0);
@@ -28,30 +28,33 @@ function setup() {
   smallAngle = PI / 2;
   midR = smallR * 1.5;
   midAngle = PI / 2;
-  rand = random(0, 360);
   colorB.mousePressed(changeBG);
 }
 
 function changeBG() {
-  val = [random(0, 360), random(70, 100), random(30, 50), 0.09];
+  backgroundVal = [random(0, 360), random(90, 100), random(30, 50), 0.09];
+  lineVal = [random(0, 360), 100, 100];
 }
 
 function draw() {
-  background(val[0], val[1], val[2], val[3]);
+  background(backgroundVal[0], backgroundVal[1], backgroundVal[2], backgroundVal[3]);
   translate(width / 2, height / 2);
-  strokeWeight(4);
-  stroke(rand, 100, 100);
+  strokeWeight(2);
+  stroke(lineVal[0], lineVal[1], lineVal[2]);
   let smallx = smallR * cos(smallAngle);
   let smally = smallR * sin(smallAngle);
   let midx = midR * cos(midAngle);
   let midy = midR * sin(midAngle);
   let bigx = bigR * cos(bigAngle);
   let bigy = bigR * sin(bigAngle);
-  beginShape(LINES);
-  vertex(smallx, smally);
-  vertex(midx, midy);
-  vertex(midx, midy);
-  vertex(bigx, bigy);
+  noFill();
+  beginShape();
+  curveVertex(mouseX-(width/2), mouseY-(height/2));
+  curveVertex(mouseX-(width/2), mouseY-(height/2));
+  curveVertex(smallx, smally);
+  curveVertex(midx, midy);
+  curveVertex(bigx, bigy);
+  curveVertex(bigx, bigy);
   endShape();
   smallAngle += 0.01 * smallSlider.value();
   midAngle += 0.01 * midSlider.value();
